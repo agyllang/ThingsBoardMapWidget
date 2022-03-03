54596cc67804bcb012d5651d715283adc3f2b918
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-03 16:34:28 +0100

    test

--- dist/map.js
+++ dist/map.js
@@ -1,4 +1,4 @@
-function $(selector)
+// function $(selector)
 // {
 //   const self = {
 //     element: document.querySelector(selector),
@@ -20,7 +20,12 @@ function $(selector)
 //   return self
 // }
 console.log("map.js script is runned")
-function initialize() {
+(function () {
+var Map,
+slice = [].slice,
+    hasProp = {}.hasOwnProperty,
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   // Create a LatLng object
   // We use this LatLng object to center the map and position the marker
@@ -34,14 +39,29 @@ function initialize() {
   };
 
   // Create a map in the #map HTML element, using the declared options
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  Map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   // Create a marker and place it on the map
   var marker = new google.maps.Marker({
       position: center,
       map: map
   });
-}
+  if (typeof window.define === 'function' && (window.define.amd != null)) {
+    define(function() {
+      return {
+        Map: Map,
+       
+      };
+    });
+  } else if (typeof module !== 'undefined' && (module.exports != null)) {
+    module.exports = {
+      Map: Map,
+    };
+  } else {
+    window.Map = Map;
+
+  }
+}).call(this)
 // // Example library calls
 // $('h1').attr('class', 'new-class')
 
