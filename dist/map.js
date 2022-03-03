fa71f404ef84ff6af9aa103a1cee707e9de73ea5
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-03 16:43:00 +0100

    Update map.js

--- dist/map.js
+++ dist/map.js
@@ -19,43 +19,43 @@
 
 //   return self
 // }
-console.log("map.js script is runned")
-function $() {
-var Map
+console.log("map.js script is runned");
+
+function $(selector) {
+  console.log("selector",selector);
+  var Map;
 
   // Create a LatLng object
   // We use this LatLng object to center the map and position the marker
-  var center = new google.maps.LatLng(50,0);
+  var center = new google.maps.LatLng(50, 0);
 
   // Declare your map options
   var mapOptions = {
-      zoom: 4,
-      center: center,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 4,
+    center: center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
 
   // Create a map in the #map HTML element, using the declared options
-  Map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  Map = new google.maps.Map(selector, mapOptions);
 
   // Create a marker and place it on the map
   // var marker = new google.maps.Marker({
   //     position: center,
   //     map: map
   // });
-  if (typeof window.define === 'function' && (window.define.amd != null)) {
-    define(function() {
+  if (typeof window.define === "function" && window.define.amd != null) {
+    define(function () {
       return {
         Map: Map,
-       
       };
     });
-  } else if (typeof module !== 'undefined' && (module.exports != null)) {
+  } else if (typeof module !== "undefined" && module.exports != null) {
     module.exports = {
       Map: Map,
     };
   } else {
     window.Map = Map;
-
   }
 }
 // // Example library calls
