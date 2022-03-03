867f6bac59ca2ce5a8d202f63e4af3dbaa581204
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-03 16:24:13 +0100

    test with map example

--- dist/map.js
+++ dist/map.js
@@ -1,25 +1,47 @@
 function $(selector)
-{
-  const self = {
-    element: document.querySelector(selector),
-    html: ()=> self.element,
-    on: (event, callback) => {
-      self.element.addEventListener(event, callback)
-    },
-    hide: ()=> {
-      self.element.style.display = 'none'
-    },
-    attr: (name, value) => {
-      if(value == null)
-        return self.element.getAttribute(name)
-      else
-        self.element.setAttribute(name, value)
-    }
-  }
+// {
+//   const self = {
+//     element: document.querySelector(selector),
+//     html: ()=> self.element,
+//     on: (event, callback) => {
+//       self.element.addEventListener(event, callback)
+//     },
+//     hide: ()=> {
+//       self.element.style.display = 'none'
+//     },
+//     attr: (name, value) => {
+//       if(value == null)
+//         return self.element.getAttribute(name)
+//       else
+//         self.element.setAttribute(name, value)
+//     }
+//   }
 
-  return self
-}
+//   return self
+// }
 console.log("map.js script is runned")
+function initialize() {
+
+  // Create a LatLng object
+  // We use this LatLng object to center the map and position the marker
+  var center = new google.maps.LatLng(50,0);
+
+  // Declare your map options
+  var mapOptions = {
+      zoom: 4,
+      center: center,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  // Create a map in the #map HTML element, using the declared options
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  // Create a marker and place it on the map
+  var marker = new google.maps.Marker({
+      position: center,
+      map: map
+  });
+}
 // // Example library calls
 // $('h1').attr('class', 'new-class')
 
