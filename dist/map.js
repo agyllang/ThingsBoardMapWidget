c2142f1da1d9e3efb295701e83ede3594b5ea115
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-03 16:39:23 +0100

    Update map.js

--- dist/map.js
+++ dist/map.js
@@ -21,11 +21,7 @@
 // }
 console.log("map.js script is runned")
 (function () {
-var Map,
-slice = [].slice,
-    hasProp = {}.hasOwnProperty,
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+var Map
 
   // Create a LatLng object
   // We use this LatLng object to center the map and position the marker
@@ -42,25 +38,25 @@ slice = [].slice,
   Map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   // Create a marker and place it on the map
-  var marker = new google.maps.Marker({
-      position: center,
-      map: map
-  });
-  if (typeof window.define === 'function' && (window.define.amd != null)) {
-    define(function() {
-      return {
-        Map: Map,
+  // var marker = new google.maps.Marker({
+  //     position: center,
+  //     map: map
+  // });
+  // if (typeof window.define === 'function' && (window.define.amd != null)) {
+  //   define(function() {
+  //     return {
+  //       Map: Map,
        
-      };
-    });
-  } else if (typeof module !== 'undefined' && (module.exports != null)) {
-    module.exports = {
-      Map: Map,
-    };
-  } else {
-    window.Map = Map;
+  //     };
+  //   });
+  // } else if (typeof module !== 'undefined' && (module.exports != null)) {
+  //   module.exports = {
+  //     Map: Map,
+  //   };
+  // } else {
+  //   window.Map = Map;
 
-  }
+  // }
 }).call(this)
 // // Example library calls
 // $('h1').attr('class', 'new-class')
