192e6526f3ccf09ee163174d366788ecdc4b72c0
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-11 15:16:08 +0100

    Update map.js

--- dist/map.js
+++ dist/map.js
@@ -57,39 +57,3 @@ class GoogleMapsApi {
     }
   }
 }
-
-
-//function $(selector)
-// {
-//   const self = {
-//     element: document.querySelector(selector),
-//     html: ()=> self.element,
-//     on: (event, callback) => {
-//       self.element.addEventListener(event, callback)
-//     },
-//     hide: ()=> {
-//       self.element.style.display = 'none'
-//     },
-//     attr: (name, value) => {
-//       if(value == null)
-//         return self.element.getAttribute(name)
-//       else
-//         self.element.setAttribute(name, value)
-//     }
-//   }
-
-//   return self
-// }
-// console.log("map.js script is runned");
-// // Example library calls
-// $('h1').attr('class', 'new-class')
-
-// console.log($('h1').attr('class'))
-
-// $('h2').hide()
-
-// $('h3').on('click', function()
-// {
-//   alert("I was clicked")
-// })
-// 
