93981392239bd1b18c2d10e9f4aa5a318442dd91
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-16 15:19:01 +0100

    Create mapboxCluster.js

---
+++ dist/mapboxCluster.js
@@ -0,0 +1,220 @@
+console.log("map.js imported 33");
+class AmbiMap {
+  constructor(divIDstring, parentElement) {
+    console.log("AmbiMap constructor");
+
+    this.divElement = document.createElement("div");
+
+    this.divElement.id = divIDstring;
+
+    this.divElement.innerHTML = "<h1>HELLO WORLD!</h1>";
+    // this.dataSources = data;
+    parentElement.appendChild(this.divElement);
+  }
+}
+
+class MapBox {
+
+  /**
+   * Constructor set up config.
+   */
+  constructor(string, containerId) {
+    console.log("MapBox constructor", string);
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYWxiaW5hbWJpZHVjdG9yIiwiYSI6ImNsMHQ4eWFwcTA0dTYzZXFscno1NHp4M3MifQ.QrzZer4qGPc9Ck19Z6Pa9Q";
+    this.map = new mapboxgl.Map({
+      container: containerId, // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [11.981, 57.671], // starting position [lng, lat]
+      zoom: 5, // starting zoom
+    });
+  }
+  init() {
+    return this.map;
+  }
+  setPin(lngLat) {
+    console.log("setPin() param:", lngLat);
+    //LngLat=[11.9810, 57.6717]
+    //göteborg
+    let m = new mapboxgl.Marker().setLngLat(lngLat).addTo(this.map);
+  }
+
+  // customMarker(meterArray) {
+  //     //meterArray - array with all the meters to be displayed
+  //   this.map.on("load", () => {
+  //     // Add an image to use as a custom marker
+  //     map.loadImage(
+  //       "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+  //       (error, image) => {
+  //         if (error) throw error;
+  //         map.addImage("custom-marker", image);
+
+  //         // Add a GeoJSON source with 2 points
+  //         map.addSource("points", {
+  //           type: "geojson",
+  //           data: {
+  //             type: "FeatureCollection",
+
+  //             features: [
+  //               {
+  //                 // feature for Mapbox DC
+  //                 type: "Feature",
+  //                 geometry: {
+  //                   type: "Point",
+  //                   coordinates: [-77.03238901390978, 38.913188059745586],
+  //                 },
+  //                 properties: {
+  //                   title: "Mapbox DC",
+  //                 },
+  //               },
+                
+  //             ],
+  //           },
+  //         });
+
+  //         // Add a symbol layer
+  //         map.addLayer({
+  //           id: "points",
+  //           type: "symbol",
+  //           source: "points",
+  //           layout: {
+  //             "icon-image": "custom-marker",
+  //             // get the title name from the source's "title" property
+  //             "text-field": ["get", "title"],
+  //             "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+  //             "text-offset": [0, 1.25],
+  //             "text-anchor": "top",
+  //           },
+  //         });
+  //       }
+  //     );
+  //   });
+  // }
+  cluster (){
+  this.map.on('load', () => {
+    // Add a new source from our GeoJSON data and
+    // set the 'cluster' option to true. GL-JS will
+    // add the point_count property to your source data.
+    this.map.addSource('earthquakes', {
+        type: 'geojson',
+        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+        data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
+   
+    this.map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'earthquakes',
+        filter: ['has', 'point_count'],
+        paint: {
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                100,
+                '#f1f075',
+                750,
+                '#f28cb1'
+            ],
+            'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40
+            ]
+        }
+    });
+
+    this.map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'earthquakes',
+        filter: ['has', 'point_count'],
+        layout: {
+            'text-field': '{point_count_abbreviated}',
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12
+        }
+    });
+
+    this.map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'earthquakes',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff'
+        }
+    });
+
+    // inspect a cluster on click
+    this.map.on('click', 'clusters', (e) => {
+        const features = this.map.queryRenderedFeatures(e.point, {
+            layers: ['clusters']
+        });
+        const clusterId = features[0].properties.cluster_id;
+        this.map.getSource('earthquakes').getClusterExpansionZoom(
+            clusterId,
+            (err, zoom) => {
+                if (err) return;
+
+                this.map.easeTo({
+                    center: features[0].geometry.coordinates,
+                    zoom: zoom
+                });
+            }
+        );
+    });
+
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    this.map.on('click', 'unclustered-point', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const mag = e.features[0].properties.mag;
+        const tsunami =
+            e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(
+                `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+            )
+            .addTo(map);
+    });
+
+    this.map.on('mouseenter', 'clusters', () => {
+        this.map.getCanvas().style.cursor = 'pointer';
+    });
+    this.map.on('mouseleave', 'clusters', () => {
+        this.map.getCanvas().style.cursor = '';
+    });
+
+});
+}
+}
+
