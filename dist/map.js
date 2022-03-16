c0d38ab397fd0d3a298a1eb033032875d9b80858
Albin Matson Gyllang <albingyllang@gmail.com>
2022-03-16 09:53:59 +0100

    Update map.js

--- dist/map.js
+++ dist/map.js
@@ -19,12 +19,12 @@ class MapBox {
   /**
    * Constructor set up config.
    */
-  constructor(string) {
+  constructor(string, containerId) {
     console.log("MapBox constructor",string)
 
   //mapboxgl.accessToken = 'pk.eyJ1IjoiYWxiaW5hbWJpZHVjdG9yIiwiYSI6ImNsMHQ4eWFwcTA0dTYzZXFscno1NHp4M3MifQ.QrzZer4qGPc9Ck19Z6Pa9Q';
   this.map= new mapboxgl.Map({
-  container: 'map', // container ID
+  container: containerId, // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
   center: [-74.5, 40], // starting position [lng, lat]
   zoom: 9 // starting zoom
